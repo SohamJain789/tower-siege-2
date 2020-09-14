eb88b7721bd5bc97734daae97b6c4bcc906d72c2
SohamJain789 <69146549+SohamJain789@users.noreply.github.com>
2020-09-14 13:44:49 +0530

    Update Box.js

--- Box.js
+++ Box.js
@@ -8,14 +8,15 @@ class Box {
     this.body = Bodies.rectangle(x, y, width, height, options);
     this.width = width;
     this.height = height;
-    this.visibility = 255;
+    this.Visibility = 255;
     World.add(world, this.body);
   }
   display(){
     var pos =this.body.position;
     var angle = this.body.angle;
-    push();
+   
     if(this.body.speed < 3){
+         push();
     translate(pos.x, pos.y);
     rotate(angle);
     rectMode(CENTER);
@@ -26,9 +27,9 @@ class Box {
     else{
     World.remove(world,this.body)
     push();
-    this.visibility = this.visibility - 5;
-    tint(255,this.visibility);
-    image(this.image,this.body.position,this.body.position.y,50,50);
+    this.Visibility = this.visibility - 5;
+    //tint(255,this.visibility);
+    //image(this.image,this.body.position,this.body.position.y,50,50);
     pop()
 }
     
